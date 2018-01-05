c4d4a9d7a66170b75d4ed314746d2cb38213b8b1
Ruud van Engelenhoven <ruud@Ruuds-MacBook-Pro.local>
2018-01-06 01:47:50 +0400

    Added basic files to start project

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+        fs: 'empty'
+    };
+
+    return config;
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Head from 'shared/components/Head/Head.js';
+
+export default () => (
+    <div>
+        <Head title="Home" />
+    </div>
+);
